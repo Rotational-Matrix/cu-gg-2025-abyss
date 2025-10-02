efdc455000dea191ad50abdd27da8a522cb144c9
Rotational-Matrix <coopermorris24@gmail.com>
2025-10-01 20:20:01 -0400

    Initial commit for dialgue box
    I know we may swap to 3d and then import 2d sprites (among other things), but I'm still going to put this in here. As long as 'Cooper's-branch' remains safe, I can always copy the contents I created into the new unity file.

---
+++ Assets/Scripts/ChoiceBoxHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This code should handle all the components of 'choicebox'
+/// 
+/// Right now, intended to handle 
+///     - the choice 'string'
+///     - whether it is selected (and the visuals for this)
+///     
+/// Also as of now, built for up/down selection of choices, NOT MOUSE_CLICK
+///     this may be easily rectified should mouse_click be desired.
+/// </summary>
+
+public class ChoiceBoxHandler : MonoBehaviour
+{
+    [SerializeField] TMPro.TMP_Text ChoiceText; // The actual displayed text in the text box
+    [SerializeField] GameObject SelectorImage; // should be a child of the Choice box panel
+    //private string internalString = ""; // maybe needed for later to parse commands
+    private bool isSelected = false;
+    //private isSelectable // only to be added in case of special plot circumstance
+
+    public void SetSelected(bool setSelected)
+    {
+        this.isSelected = setSelected;
+        SelectorImage.SetActive(isSelected);
+    }
+    public bool IsSelected()
+    {
+        return isSelected;
+    }
+
+    public void setTextContents(string newContents)
+    {
+        ChoiceText.text = newContents;
+    }
+
+    public void cleanTextContents()
+    {
+        ChoiceText.text = "";
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Dialogue_canvas_manager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_canvas_manager : MonoBehaviour
+{
+    // L + Ratio I haven't implemented anything yet
+    /* The plan is to make this the top-level communicator for the whole Dialogue_Canvas
+     * So observe the following lower-level files
+     *  - The Choice Canvas should handle all choice boxes collectively
+     *      - ChoiceBoxHandler will be present for each ChoiceBox
+     * 
+     * This should be the code that does the following:
+     *  - communicates and directs the ink file (obtians strings and #tags from it, too)
+     *  - parse strings from said ink file for commands
+     *  - populate the main dialogue body
+     *      - should handle text crawl and text speed-the-hell-up (i.e. spontaneously ending text crawl)
+     *  - fill in the sprites of the speakers (or lacktherof)
+     *  - perish
+     *  - play sound for dialogue/narration (???)
+     *  - Activates/disables the choice canvas, sends the choice strings and receives the choice index
+     */
+}
