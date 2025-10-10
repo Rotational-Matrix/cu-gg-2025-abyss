59ddd62018e80b6d6c18b467cafe30461c522219
John Mitnik <johnmitnik2@gmail.com>
2025-10-10 19:54:26 -0400

    Add physics demo, 2.5D platforming

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private int groundedFrameCount;
+    private Vector2 inputVector;
+    SpriteRenderer spriteRenderer;
+    private float oldX;
+    private Vector3 force;
+    private Rigidbody rb;
+    public float movementSpeed;
+    public float maxSpeed;
+    public float jumpSpeed;
+    PlayerInputActions inputActions;
+    InputAction move;
+
+    /*void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        grounded = false;
+    }*/
+    bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
+    }
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnEnable()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Land.Jump.performed += OnJump;
+        move = inputActions.Land.Movement;
+        inputActions.Enable();
+    }
+    void OnDisable()
+    {
+        inputActions.Land.Jump.performed -= OnJump;
+        inputActions.Disable();
+    }
+    void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (isGrounded()) rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (move == null) return;
+        inputVector = move.ReadValue<Vector2>();
+        if (spriteRenderer != null)
+        {
+            if (inputVector.x >= 0.05) spriteRenderer.flipX = false;
+            else if (inputVector.x <= -0.05) spriteRenderer.flipX = true;
+        }
+        if (isGrounded() && Mathf.Abs(inputVector.x) < 0.05 && Mathf.Abs(inputVector.y) < 0.05) groundedFrameCount++;
+        else groundedFrameCount = 0;
+        if (groundedFrameCount >= 100 && groundedFrameCount % 59 == 0) spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+    void FixedUpdate()
+    {
+       
+        if (inputVector.x * oldX < 0) spriteRenderer.flipX = !spriteRenderer.flipX;
+        Vector3 direction = (transform.right * inputVector.x + transform.forward * inputVector.y);
+        rb.AddForce(direction * movementSpeed, ForceMode.VelocityChange);
+        rb.AddForce(force);
+        oldX = inputVector.x;
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
+        if (flatVelocity.magnitude > maxSpeed)
+        {
+            Vector3 clamped = flatVelocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(clamped.x, rb.velocity.y, clamped.z);
+        }
+    }
+}
