50c1ec397ad93408f9c97fedb2f93416af8ebcd7
Shanying Liu <jl200850984@gmail.com>
2025-11-18 19:28:02 -0500

    Changed flower behaviour, made prefab, added vfx

--- Assets/Scripts/Puzzles/Flower.cs
+++ Assets/Scripts/Puzzles/Flower.cs
@@ -4,19 +4,76 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
+    [Header("Manager")]
     public GameObject managerParent;
     private FlowerManager manager;
-    // Start is called before the first frame update
+
+    [Header("Sprites")]
+    public List<Sprite> possibleSprites = new List<Sprite>();
+    private SpriteRenderer spriteRenderer;
+
+    [Header("Particles")]
+    private ParticleSystem particleSystem;
+
+    private bool picked = false;
+    private Collider col;
+
     void Start()
     {
         manager = managerParent.GetComponent<FlowerManager>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("Flower: No SpriteRenderer found.");
+        }
+        else
+        {
+            if (possibleSprites.Count > 0)
+            {
+                spriteRenderer.sprite = possibleSprites[Random.Range(0, possibleSprites.Count)];
+            }
+
+            if (Random.value < 0.5f)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        col = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("hi");
+        if (picked) return;
+        picked = true;
+
+        if (col != null) col.enabled = false;
+        if (spriteRenderer != null) spriteRenderer.enabled = false;
+
         manager.AddFlower();
+
+        if (particleSystem != null)
+        {
+            ParticleSystem p = Instantiate(particleSystem, transform.position, Quaternion.identity);
+            p.Play();
+            Destroy(p.gameObject, p.main.duration + p.main.startLifetime.constantMax);
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator DisableAfterParticles()
+    {
+        if (particleSystem != null)
+        {
+            while (particleSystem.isPlaying)
+            {
+                yield return null;
+            }
+        }
+
         this.gameObject.SetActive(false);
     }
 }
